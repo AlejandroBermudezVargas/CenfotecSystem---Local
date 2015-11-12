8fd5171f309ab48126c4e22504fd0746ffe7b982
unknown <abermudezv@ucenfotec.ac.cr>
2015-11-11 22:31:48 -0600

    Remove excel related methods

--- PresentasionWindowsForms/Controllers/ProductsController.vb
+++ PresentasionWindowsForms/Controllers/ProductsController.vb
@@ -1,6 +1,6 @@
 ﻿Imports System.Configuration
 Imports RestSharp
-Imports Microsoft.Office.Interop
+'Imports Microsoft.Office.Interop
 Imports Newtonsoft.Json
 
 Public Class ProductsController
@@ -26,62 +26,62 @@ Public Class ProductsController
 
     End Function
 
-    Shared Sub ImportarProductos(ByVal path As String)
-        Dim APP As New Excel.Application
-        Dim worksheet As Excel.Worksheet
-        Dim workbook As Excel.Workbook
-        Dim rango As Excel.Range
-        Dim rowNum As Integer
-        Dim fieldNum As Integer
-        Dim rowCount As Integer
-        Dim colCount As Integer
-        Dim codProd As String
-        Dim nombreProd As String
-        Dim costoProd As Double
-        Dim costo2 As String
-        Dim tipoProd As Integer
-        Dim horario As String
+    'Shared Sub ImportarProductos(ByVal path As String)
+    '    Dim APP As New Excel.Application
+    '    Dim worksheet As Excel.Worksheet
+    '    Dim workbook As Excel.Workbook
+    '    Dim rango As Excel.Range
+    '    Dim rowNum As Integer
+    '    Dim fieldNum As Integer
+    '    Dim rowCount As Integer
+    '    Dim colCount As Integer
+    '    Dim codProd As String
+    '    Dim nombreProd As String
+    '    Dim costoProd As Double
+    '    Dim costo2 As String
+    '    Dim tipoProd As Integer
+    '    Dim horario As String
 
-        horario = ""
-        nombreProd = ""
-        costoProd = 0
-        codProd = ""
-        workbook = APP.Workbooks.Open(path)
-        worksheet = workbook.Worksheets(1)
-        rango = worksheet.UsedRange
-        rowNum = 1
-        rowCount = worksheet.UsedRange.Rows.Count
-        colCount = worksheet.UsedRange.Columns.Count
+    '    horario = ""
+    '    nombreProd = ""
+    '    costoProd = 0
+    '    codProd = ""
+    '    workbook = APP.Workbooks.Open(path)
+    '    worksheet = workbook.Worksheets(1)
+    '    rango = worksheet.UsedRange
+    '    rowNum = 1
+    '    rowCount = worksheet.UsedRange.Rows.Count
+    '    colCount = worksheet.UsedRange.Columns.Count
 
-        MsgBox(rowCount)
-        For i = 1 To rowCount
-            For j = 1 To colCount
-                fieldNum = j
-                Select Case (fieldNum)
-                    Case 1
-                        codProd = rango.Cells(i, j).Value.ToString()
-                    Case 2
-                        nombreProd = rango.Cells(i, j).Value.ToString()
-                    Case 3
-                        costo2 = rango.Cells(i, j).Value.ToString()
-                        If (costo2.Equals("Costo")) Then
-                        Else
-                            costoProd = Double.Parse(costo2)
-                        End If
-                    Case 4
-                        tipoProd = Integer.Parse(rango.Cells(i, j).Value.ToString())
-                    Case 5
-                        horario = rango.Cells(i, j).Value.ToString()
-                    Case Else
-                End Select
-            Next j
-            RegistrarProducto(nombreProd, costoProd, codProd, horario, tipoProd)
-        Next i
-        costoProd = 0
-        codProd = ""
-        nombreProd = ""
-        workbook.Close()
-    End Sub
+    '    MsgBox(rowCount)
+    '    For i = 1 To rowCount
+    '        For j = 1 To colCount
+    '            fieldNum = j
+    '            Select Case (fieldNum)
+    '                Case 1
+    '                    codProd = rango.Cells(i, j).Value.ToString()
+    '                Case 2
+    '                    nombreProd = rango.Cells(i, j).Value.ToString()
+    '                Case 3
+    '                    costo2 = rango.Cells(i, j).Value.ToString()
+    '                    If (costo2.Equals("Costo")) Then
+    '                    Else
+    '                        costoProd = Double.Parse(costo2)
+    '                    End If
+    '                Case 4
+    '                    tipoProd = Integer.Parse(rango.Cells(i, j).Value.ToString())
+    '                Case 5
+    '                    horario = rango.Cells(i, j).Value.ToString()
+    '                Case Else
+    '            End Select
+    '        Next j
+    '        RegistrarProducto(nombreProd, costoProd, codProd, horario, tipoProd)
+    '    Next i
+    '    costoProd = 0
+    '    codProd = ""
+    '    nombreProd = ""
+    '    workbook.Close()
+    'End Sub
     Shared Function obtenerListaProductos() As List(Of Producto)
         Dim client = New RestClient(ConfigurationManager.AppSettings.Get("endpoint"))
         Dim request = New RestRequest("Products", Method.GET)
